53237d1ad260fa3cdc0adacd5d2d15c164d69632
schondell <per@schondell.com>
2025-05-10 21:22:08 +0200

    Update to openId

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://coordinatewebapp.azurewebsites.net', // Change this to the address of your backend API if different from frontend address
+  baseUrl: 'https://coordinatewebapi.mangoplant-ee517a16.switzerlandnorth.azurecontainerapps.io', // Change this to the address of your backend API if different from frontend address
   loginUrl: '/login',
   googleClientId: null,
   facebookClientId: null,
-  tokenUrl: 'https://coordinatewebapp.azurewebsites.net/connect/token',
+  tokenUrl: 'https://coordinatewebapi.mangoplant-ee517a16.switzerlandnorth.azurecontainerapps.io', // Base URL without /connect/token - this is added in the endpoint factory
   microsoftClientId: null
 };
